670c6030124395693067af2cb82fe3f542a5bf71
maitc <maibeet.torres@gmail.com>
2017-07-05 11:58:10 -0400

    ejercicio ok

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+window.fbAsyncInit = function() {
+	    FB.init({
+	      appId      : '1616022411750190',
+	      cookie     : true,
+	      xfbml      : true,
+	      version    : 'v2.8'
+	    });
+	    FB.AppEvents.logPageView();   
+	  };
+
+	  (function(d, s, id){
+	     var js, fjs = d.getElementsByTagName(s)[0];
+	     if (d.getElementById(id)) {return;}
+	     js = d.createElement(s); js.id = id;
+	     js.src = "//connect.facebook.net/en_US/sdk.js";
+	     fjs.parentNode.insertBefore(js, fjs);
+	   }(document, 'script', 'facebook-jssdk'));
+
+	  
+FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+});
+
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
